3c2fb26b8348ca6dd9f69fc7ba5c79984bd71a6a
unknown <ckelly28@myseneca.ca>
2020-05-05 17:29:00 -0400

    Added my Ionic App

--- routes/apps.js
+++ routes/apps.js
@@ -5,4 +5,12 @@ route.get('/', (req,res)=>{
     res.render('appProjects/appOverview');
 })
 
+route.get('/eatme', (req, res)=>{
+    res.render('appProjects/eatMeOverview')
+})
+
+route.get('/eatMeSite', (req, res)=>{
+    res.redirect('http://eatme-public.herokuapp.com')
+})
+
 module.exports = route;
